ef194be1625f1fea6e5ed543de5df5a0dc7ba8a4
erosy <39221189+erosy@users.noreply.github.com>
2021-10-05 11:35:55 +0700

    Update 5 Oktober 2021

---
+++ Assets/PlatformerRaycast/Script/Bullet.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed;
+    //private float direction;
+    public float bulletDuration;
+    private float bulletTimer;
+    private Rigidbody2D rb;
+
+    private void OnEnable()
+    {
+        bulletTimer = bulletDuration;
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = Vector2.left /* direction*/ * speed;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        bulletTimer = bulletDuration;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = Vector2.left /* direction*/ * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //direction = Mathf.Sign(transform.localScale.x);
+        bulletTimer -= Time.deltaTime;
+        if (bulletTimer <= 0)
+            gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("CameraTrigger"))
+            bulletTimer = 0;
+    }
+}
---
+++ Assets/PlatformerRaycast/Script/ObjectPool.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+    public static ObjectPool instance;
+    private List<GameObject> pooledObjects = new List<GameObject>();
+    private int amountToPool = 20;
+
+    public GameObject bulletPrefab;
+
+    private void Awake()
+    {
+        if(instance == null)
+            instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < amountToPool; i++)
+        {
+            GameObject obj = Instantiate(bulletPrefab, this.transform);
+            AddObjectToList(obj);
+            obj.SetActive(false);
+        }
+            
+    }
+
+    public GameObject GetPooledObject()
+    {
+        for (int i = 0; i < pooledObjects.Count; i++)
+        {
+            if (!pooledObjects[i].activeInHierarchy)
+            {
+                return pooledObjects[i];
+            }
+        }
+
+        return null;
+        
+    }
+
+    public void AddObjectToList(GameObject obj)
+    {
+        pooledObjects.Add(obj);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
